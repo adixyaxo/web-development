bd551b93f607642c2c50fea3ee65c1a3134325d6
adixyaxo <aditya0dagar@gmail.com>
2026-07-20 21:11:37 +0530

    creating custom react

---
+++ React_Projects/01basicviteapp/src/Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component = () => {
+  return (
+    <>
+    <h1>This component has been injected by me</h1>
+    </>
+  )
+}
+
+export default Component
---
+++ React_Projects/customReact/script.js
@@ -0,0 +1,21 @@
+const reactElement = {
+                        type: 'a',
+                        props: {
+                          href: 'https://google.com',
+                          target: '_blank'
+                        },
+                        children: 'Click me to visit google'
+                    }
+
+
+function customRender(reactElement,container) {
+  const domElement = document.createElement(reactElement.type)
+  domElement.innerHTML = reactElement.children
+  domElement.setAttribute('href',reactElement.props.href)
+  domElement.setAttribute('target',reactElement.props.target)
+  container.appendChild(domElement)
+}
+
+const body = document.querySelector("#root")
+
+customRender(reactElement,body)
